a20d1404d51a68e15476f5f8fde1be64200eecde
byokurt <osman.kurt@msn.com>
2020-12-07 21:56:37 +0300

    dotnet core httpclientfactory usage

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HttpClientFactoryExample.Helper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientFactoryExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ExampleHttpClient _exampleHttpClient;
+
+        public HomeController(IHttpClientFactory httpClientFactory, ExampleHttpClient exampleHttpClient)
+        {
+            _httpClientFactory = httpClientFactory;
+            _exampleHttpClient = exampleHttpClient;
+        }
+
+        [HttpGet]
+        [Route("exampleone")]
+        public async Task<string> ExampleOne()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("http://www.osmankurt.net");
+            var result = await client.GetStringAsync("/");
+            return result;
+        }
+
+        [HttpGet]
+        [Route("exampletwo")]
+        public async Task<string> ExampleTwo()
+        {
+            var client = _httpClientFactory.CreateClient("microsoft");
+            client.BaseAddress = new Uri("http://www.osmankurt.net");
+            var result = await client.GetStringAsync("/");
+            return result;
+        }
+
+        [HttpGet]
+        [Route("examplethree")]
+        public async Task<string> ExampleThree()
+        {
+            var result = await _exampleHttpClient.Client.GetStringAsync("/");
+            return result;
+        }
+    }
+}
---
+++ Helper/ExampleHttpClient.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net.Http;
+
+namespace HttpClientFactoryExample.Helper
+{
+    public class ExampleHttpClient
+    {
+        public HttpClient Client { get; private set; }
+
+        public ExampleHttpClient(HttpClient httpClient)
+        {
+            httpClient.BaseAddress = new Uri("https://www.osmankurt.net/");
+            httpClient.DefaultRequestHeaders.Add("ExampleHeaderKey", "for-example-three");
+            Client = httpClient;
+        }
+    }
+}
